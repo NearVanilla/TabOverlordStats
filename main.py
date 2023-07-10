55e8e987cd248892c8aba7b2d442332efbbc8560
Kiddooo <29415728+Kiddooo@users.noreply.github.com>
2023-07-10 16:47:09 +0100

    reformatted

--- main.py
+++ main.py
@@ -6,34 +6,35 @@ from utils import make, blacklists
 from utils.minecraft_data import minecraft_data
 
 
-def extract(version):
+def extract(version: str):
     if not os.path.exists(f"{version}.json"):
         print(f"Extracting data for version {version}")
         subprocess.run(["python", Path.joinpath(Path.cwd().joinpath("Burger/munch.py")), "--download", version, "--toppings", "blocks,entities,items,recipes", "-o", f"{Path.joinpath(Path.cwd(), 'data')}/{version}.json"])
         os.remove(Path(os.getcwd(), f"{version}.jar"))
-        print_hi()
+        start()
 
 
 class Main:
     def __init__(self, version):
+
         self.version = version
         with open(f"{Path.joinpath(Path.cwd(), 'data')}/{version}.json", "r") as version_file:
             self.mcdata = minecraft_data.from_json(version_file.read())
 
-        self.make = make.Make(self.mcdata)
-        self.custom_stats = self.mcdata.language["stat"]
+            self.make = make.Make(self.mcdata)
+            self.custom_stats = self.mcdata.language["stat"]
 
-        self.make_mined = self.make.create_make_function(blacklists.mined_blacklist + blacklists.default_blacklist)
-        self.make_crafted = self.make.create_make_function(blacklists.craftable_blacklist + blacklists.default_blacklist, has_recipe=True)
-        self.make_broken = self.make.create_make_function(None, whitelist=blacklists.breakable_items)
-        self.make_dropped = self.make.create_make_function(blacklists.dropped_blacklist + blacklists.netherite + blacklists.diamond + blacklists.iron + blacklists.gold + blacklists.chainmail + blacklists.leather + blacklists.wooden + blacklists.stone)
-        self.make_picked_up = self.make.create_make_function(blacklists.picked_up_blacklist + blacklists.default_blacklist)
-        self.make_killed = self.make.create_make_function(None, is_entity=True, whitelist=blacklists.killed_whitelist)
-        self.make_killed_by = self.make.create_make_function(None, is_entity=True, whitelist=blacklists.killed_by_whitelist)
-        self.make_custom = self.make.create_make_function(blacklists.custom_blacklist + blacklists.default_blacklist, is_custom=True)
+            self.make_mined = self.make.create_make_function(blacklists.mined_blacklist + blacklists.default_blacklist)
+            self.make_crafted = self.make.create_make_function(blacklists.craftable_blacklist + blacklists.default_blacklist, has_recipe=True)
+            self.make_broken = self.make.create_make_function(None, whitelist=blacklists.breakable_items)
+            self.make_dropped = self.make.create_make_function(blacklists.dropped_blacklist + blacklists.netherite + blacklists.diamond + blacklists.iron + blacklists.gold + blacklists.chainmail + blacklists.leather + blacklists.wooden + blacklists.stone)
+            self.make_picked_up = self.make.create_make_function(blacklists.picked_up_blacklist + blacklists.default_blacklist)
+            self.make_killed = self.make.create_make_function(None, is_entity=True, whitelist=blacklists.killed_whitelist)
+            self.make_killed_by = self.make.create_make_function(None, is_entity=True, whitelist=blacklists.killed_by_whitelist)
+            self.make_custom = self.make.create_make_function(blacklists.custom_blacklist + blacklists.default_blacklist, is_custom=True)
 
 
-def print_hi():
+def start():
     version = "1.20.1"
 
     if not Path.exists(Path.joinpath(Path.cwd(), f"{Path.joinpath(Path.cwd(), 'data')}/{version}.json")):
@@ -58,4 +59,4 @@ def print_hi():
 
 
 if __name__ == '__main__':
-    print_hi()
+    start()
